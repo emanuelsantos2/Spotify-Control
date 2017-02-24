703fcb9f7a5404850a756bdbb11601dcc1506b47
Emanuel Santos <emaeva4@gmail.com>
2017-02-24 16:13:26 +0000

    1.0.1

---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hotkeys;
+
+namespace Spotify_Remote
+{
+    public partial class Form1 : Form
+    {
+        HandleKey play, previous, next;
+        SpotifyControl spotify;
+        public Form1()
+        {
+            InitializeComponent();
+            spotify = new SpotifyControl();
+            play = new HandleKey(Constants.ALT + Constants.SHIFT, Keys.F10, this);
+            previous = new HandleKey(Constants.ALT + Constants.SHIFT, Keys.F11, this);
+            next = new HandleKey(Constants.ALT + Constants.SHIFT, Keys.F12, this);
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+            try
+            {
+                if (m.WParam.ToInt32() == play.GetHashCode())
+                {
+                    spotify.SendCommand((long)SpotifyControl.SpotifyAction.PlayPause);
+                    WriteLine("Play/Pause");
+                }
+
+                if (m.WParam.ToInt32() == next.GetHashCode())
+                {
+                    spotify.SendCommand((long)SpotifyControl.SpotifyAction.NextTrack);
+                    WriteLine("Next");
+                }
+
+                if (m.WParam.ToInt32() == previous.GetHashCode())
+                {
+                    spotify.SendCommand((long)SpotifyControl.SpotifyAction.PreviousTrack);
+                    WriteLine("Previous");
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+                
+            base.WndProc(ref m);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (play.Register() && next.Register() && previous.Register())
+                WriteLine("Connected to Spotify");
+            else
+                WriteLine("Not Conected to Spotify, Try Again");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            play.Unregister();
+            next.Unregister();
+            previous.Unregister();
+            spotify.CloseSpotify();
+        }
+
+        private void WriteLine(string text)
+        {
+            label1.ResetText();
+            label1.Text = text;
+            System.Threading.Thread.Sleep(500);
+            label1.Text = "...";
+        }
+    }
+}
---
+++ SpotifyControl.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Spotify_Remote
+{
+    class SpotifyControl
+    {
+        
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+
+        public IntPtr windowHandle = IntPtr.Zero;
+        public bool isConnected = false;
+        private const uint WM_APPCOMMAND = 0x0319;
+        private Process spotifyProcess;
+
+        public enum SpotifyAction : long
+        {
+            PlayPause = 917504,
+            Mute = 524288,
+            VolumeDown = 589824,
+            VolumeUp = 655360,
+            Stop = 851968,
+            PreviousTrack = 786432,
+            NextTrack = 720896
+        }
+
+
+        public SpotifyControl()
+        {
+            this.windowHandle = GetSpotifyWindowsHandle();
+        }
+
+        private IntPtr GetSpotifyWindowsHandle()
+        {
+            bool contSearch = true;
+            IntPtr hWnd = IntPtr.Zero;
+            spotifyProcess = Process.Start("C:/Users/emaev/AppData/Roaming/Spotify/spotify.exe");
+            System.Threading.Thread.Sleep(1000);
+
+            while (contSearch)
+            {
+                Process[] processes = Process.GetProcesses();
+
+                for (int i = 0; i < processes.GetLength(0); i++)
+                {
+                    if (processes[i].MainWindowTitle.Contains("Spotify"))
+                    {
+                        hWnd = processes[i].MainWindowHandle;
+                        contSearch = false;
+                    }
+                }
+            }
+            isConnected = true;
+            return hWnd;
+        }
+
+        public void SendCommand(long command)
+        {
+            SendMessage(windowHandle, WM_APPCOMMAND, IntPtr.Zero, new IntPtr(command));
+        }
+        
+        public void CloseSpotify()
+        {
+            
+                spotifyProcess.Kill();
+            
+        }
+        
+    }
+}
+
